9a44852e471d97c411b51e5e75f895d9609b348d
Suman  Subedi <ssubedi60@gmail.com>
2024-09-12 14:36:09 -0500

    Fetched required data from JSON file

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 "use strict";
 
+const countriesContainer = document.querySelector(".countries");
+
 const request = new XMLHttpRequest();
 request.open("GET", "https://restcountries.com/v3.1/name/nepal");
 request.send();
@@ -7,4 +9,20 @@ request.send();
 request.addEventListener("load", function () {
   const [data] = JSON.parse(this.responseText);
   console.log(data);
+
+  const html = `
+  <article class="country">
+    <img class="country__img" src="${data.flags.png}" />
+    <div class="country__data">
+      <h3 class="country__name">${data.name.common}</h3>
+      <h4 class="country__region">${data.region}</h4>
+      <p class="country__row"><span>👫</span>${(
+        +data.population / 1000000
+      ).toFixed(1)} people</p>
+      <p class="country__row"><span>🗣️</span>${data.languages.nep}</p>
+      <p class="country__row"><span>💰</span>${data.currencies.NPR.name}</p>
+    </div>
+  </article>
+  `;
+  countriesContainer.insertAdjacentHTML("beforeend", html);
 });
