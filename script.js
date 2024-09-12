be007441eaf956d81e639b0da8a4c22bce7e192c
Suman  Subedi <ssubedi60@gmail.com>
2024-09-12 17:51:06 -0500

    Changed some bad coding practice

--- script.js
+++ script.js
@@ -1,28 +1,38 @@
 "use strict";
 
-const countriesContainer = document.querySelector(".countries");
+const getCountryData = function (country) {
+  const countriesContainer = document.querySelector(".countries");
 
-const request = new XMLHttpRequest();
-request.open("GET", "https://restcountries.com/v3.1/name/nepal");
-request.send();
+  const request = new XMLHttpRequest();
+  request.open("GET", `https://restcountries.com/v3.1/name/${country}`);
+  request.send();
 
-request.addEventListener("load", function () {
-  const [data] = JSON.parse(this.responseText);
-  console.log(data);
+  request.addEventListener("load", function () {
+    const [data] = JSON.parse(this.responseText);
+    console.log(data);
 
-  const html = `
+    const html = `
   <article class="country">
-    <img class="country__img" src="${data.flags.png}" />
+    <img class="country__img" src="${data.flags.svg}" />
     <div class="country__data">
       <h3 class="country__name">${data.name.common}</h3>
       <h4 class="country__region">${data.region}</h4>
       <p class="country__row"><span>👫</span>${(
         +data.population / 1000000
-      ).toFixed(1)} people</p>
-      <p class="country__row"><span>🗣️</span>${data.languages.nep}</p>
-      <p class="country__row"><span>💰</span>${data.currencies.NPR.name}</p>
+      ).toFixed(1)} million people</p>
+      <p class="country__row"><span>🗣️</span>${
+        Object.entries(data.languages)[0][1]
+      }</p>
+      <p class="country__row"><span>💰</span>${
+        Object.entries(data.currencies)[0][0]
+      }, ${Object.entries(data.currencies)[0][1].name}</p>
     </div>
   </article>
   `;
-  countriesContainer.insertAdjacentHTML("beforeend", html);
-});
+    countriesContainer.insertAdjacentHTML("beforeend", html);
+  });
+};
+
+getCountryData("nepal");
+getCountryData("india");
+getCountryData("germany");
