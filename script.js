446f38f6d9b1117a07ac3b6c4273d4e02a725850
Suman  Subedi <ssubedi60@gmail.com>
2024-09-12 14:00:06 -0500

    setting up the API call using xml

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 "use strict";
+
+const request = new XMLHttpRequest();
+request.open("GET", "https://restcountries.com/v3.1/name/nepal");
+request.send();
+
+request.addEventListener("load", function () {
+  const [data] = JSON.parse(this.responseText);
+  console.log(data);
+});
